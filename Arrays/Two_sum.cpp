8b90253cda148e8fc47c15130a42709308522f8e
PiyushKumarDewangan <piyushdewangan461@gmail.com>
2026-02-07 22:58:41 +0530

    Update Two_sum.cpp

--- Arrays/Two_sum.cpp
+++ Arrays/Two_sum.cpp
@@ -4,16 +4,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++)
-        {
-            for(int j=i+1; j<nums.size(); j++)
-            {
-               if(target==nums[i]+nums[j])
-               {
-                return {i,j};
-               }
+        unordered_map<int, int> m;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int first = nums[i], sec = target - first;
+
+            if (m.find(sec) != m.end()) {
+                ans.push_back(i);
+                ans.push_back(m[sec]);
+                break;
             }
+            m[first] = i;
         }
-        return {};
+        return ans;
     }
 };
