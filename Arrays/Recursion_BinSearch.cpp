e1facf2beddd97d0f6e090bcba787aca20da5a46
PiyushKumarDewangan <piyushdewangan461@gmail.com>
2026-02-12 22:46:15 +0530

    Add files via upload

---
+++ Arrays/Recursion_BinSearch.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int binSearch(vector<int>& arr, int target, int st, int end) {
+        if (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (arr[mid] == target)
+                {return mid;}
+            else if (arr[mid] <= target) {
+                return binSearch(arr, target, mid + 1, end);
+            } else {
+                return binSearch(arr, target, st, mid - 1);
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& arr, int target) {
+        // int st=0, end=arr.size()-1;
+        return binSearch(arr, target, 0, arr.size() - 1);
+    }
+};
