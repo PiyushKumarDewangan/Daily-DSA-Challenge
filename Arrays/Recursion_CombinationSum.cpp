80d489a4d3fbffb723428b0f870338524c1c7141
PiyushKumarDewangan <piyushdewangan461@gmail.com>
2026-02-17 21:25:09 +0530

    Add files via upload

---
+++ Arrays/Recursion_CombinationSum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+class Solution
+{
+public:
+    set<vector<int>> s;
+    void getAllCombinations(vector<int> &arr, int idx, int tar, vector<vector<int>> &ans, vector<int> &combin)
+    {
+        if (idx == arr.size() || tar < 0)
+        {
+            return;
+        }
+        if (tar == 0)
+        {
+            if (s.find(combin) == s.end())
+            {
+                ans.push_back(combin);
+                s.insert(combin);
+            }
+            return;
+        }
+        combin.push_back(arr[idx]);
+        // single
+        getAllCombinations(arr, idx + 1, tar - arr[idx], ans, combin);
+        // multiple
+        getAllCombinations(arr, idx, tar - arr[idx], ans, combin);
+        combin.pop_back();
+        // exclusion
+        getAllCombinations(arr, idx + 1, tar, ans, combin);
+    }
+    vector<vector<int>> combinationSum(vector<int> &arr, int target)
+    {
+        vector<int> combin;
+        vector<vector<int>> ans;
+
+        getAllCombinations(arr, 0, target, ans, combin);
+        return ans;
+    }
+};
