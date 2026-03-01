6bfc5c769ea9c4de6761b2dd1b3fe4b039022688
PiyushKumarDewangan <piyushdewangan461@gmail.com>
2026-03-01 13:45:12 +0530

    Add files via upload

---
+++ Arrays/MoveZeroesToEnd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int j = 0; // position for next non-zero
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0)
+            {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
